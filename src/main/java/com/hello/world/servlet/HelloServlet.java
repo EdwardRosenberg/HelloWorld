d7765c4cd34834123cfca448a782fe64b848cd3d
EdwardRosenberg <edward.rosenberg@outlook.com>
2014-03-05 16:50:20 -0600

    Added new servlet, adjusted context root.

---
+++ src/main/java/com/hello/world/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.hello.world.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		PrintWriter responseWriter = response.getWriter();
+		responseWriter.println("<h1> Hello from Servlet! </h1>");
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+
+	<h1>Hello World!</h1>
+	
+	<h2>JSP says Hello!</h2>
+	
 </body>
 </html>
